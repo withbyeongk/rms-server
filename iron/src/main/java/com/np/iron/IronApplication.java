0218cf3ce77e83833cf9855f94957d5ef63e1175
withbyeongk <76987021+withbyeongk@users.noreply.github.com>
2023-11-26 12:19:33 +0900

    init
    초기 프로젝트 업로드

---
+++ iron/src/main/java/com/np/iron/IronApplication.java
@@ -0,0 +1,13 @@
+package com.np.iron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronApplication.class, args);
+	}
+
+}
