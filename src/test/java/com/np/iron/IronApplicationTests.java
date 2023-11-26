252f6f4ca66c9afc04e6671b14722a49bf9c85c1
withbyeongk <76987021+withbyeongk@users.noreply.github.com>
2023-11-26 12:25:55 +0900

    init
    초기프로젝트파일 재업로드

---
+++ src/test/java/com/np/iron/IronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.iron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
